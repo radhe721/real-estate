e2c43ce90aeb1832ba5fb543baf57a3c2faf804e
radhe721 <radhewebtech@gmail.com>
2025-09-14 21:48:12 +0530

    Created Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import About from './pages/About';
 import SingIn from './pages/SingIn';
+import Header from './components/Header';  // Make sure Header is imported
+
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/> 
-      <Route path="/sign-in" element={<SingIn/>}/> 
-      <Route path="/sign-up" element={<Signup/>}/> 
-      <Route path="/about" element={<About/>}/> 
-    </Routes>
+      <Header /> {/* Render Header outside of Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/sign-in" element={<SingIn />} /> 
+        <Route path="/sign-up" element={<Signup />} /> 
+        <Route path="/about" element={<About />} /> 
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
